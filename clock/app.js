c99d536148c752ad186fa03a65cb9193cb073a02
rashmi ranjan <rsahoorashmi@gmail.com>
2026-05-27 11:49:53 +0530

    Day 7: Digital Clock

---
+++ clock/app.js
@@ -0,0 +1,14 @@
+let hrs=document.getElementById("hrs");
+let min=document.getElementById("min");
+let sec=document.getElementById("sec");
+
+setInterval(() => {
+    let curr=new Date();
+hrs.innerHTML=(curr.getHours()<10 ? "0" :"")+curr.getHours();
+min.innerHTML=(curr.getMinutes()<10 ? "0" :"")+curr.getMinutes();
+sec.innerHTML=(curr.getSeconds()<10 ? "0" :"")+curr.getSeconds();
+    
+}, 1000);
+
+
+
