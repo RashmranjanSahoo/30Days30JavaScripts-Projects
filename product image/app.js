abd7c20408b7c772320c6d18f2ce49933595c092
rashmi ranjan <rsahoorashmi@gmail.com>
2026-06-05 12:55:25 +0530

    day-16 Product image

---
+++ product image/app.js
@@ -0,0 +1,27 @@
+let productimg=document.getElementById("productimg");
+let btn=document.getElementsByClassName("btn");
+
+btn[0].onclick=function(){
+    productimg.src="men/image1.png";
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+
+}
+btn[1].onclick=function(){
+    productimg.src="men/image2.png";
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+    
+}
+btn[2].onclick=function(){
+    productimg.src="men/image3.png";
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+    
+}
